3468118527159ff32c8d0010ce9c01709762caa5
pospi <pospi@spadgos.com>
2013-11-16 16:36:25 +1000

    update main entrypoint

--- imthumb.php
+++ imthumb.php
@@ -22,15 +22,16 @@
 
 // Load up configuration (use same filename that timthumb does) and set defaults. Looks in CWD and parent dir.
 // @see http://www.binarymoon.co.uk/2012/03/timthumb-configs/
-if (file_exists(dirname(__FILE__) . '/timthumb-config.php'))	{
+if (file_exists(__DIR__ . '/timthumb-config.php'))	{
 	require_once('timthumb-config.php');
 }
-if (file_exists(dirname(dirname(__FILE__)) . '/timthumb-config.php'))	{
+if (file_exists(dirname(__DIR__) . '/timthumb-config.php'))	{
 	require_once('../timthumb-config.php');
 }
 
+require_once('imthumb-requesthandler.class.php');
 require_once('imthumb.class.php');
 
 // run the script!
-ImThumb::processRequest();
+ImthumbRequestHandler::processRequest();
 exit(0);
